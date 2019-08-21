d26ffa1fded5f0f6b1fab70df002833df9f51a41
Pritom Dutta <duttapritom162@gmail.com>
2019-08-21 14:07:13 +0600

    admin_home

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		
+		<h1 ><a>Welcome To Admin Panel</a></h1>
+		
+		<h1><a href="/adminhome/adminprofile">Admin Profile</a> | |
+		<a href="/adminhome/moderatorlist">Moderator List</a> ||
+		<a href="/adminhome/addmoderator"> Add Moderator</a> ||
+		<a href="/adminhome/content"> Upload Content</a> ||
+		<a href="/adminhome/movielist"> Delete Content</a> ||
+		<a href="/adminhome/viewrequest"> View Request </a> ||
+		<a href="/logout">LogOut</h1>
+	</body>
+	</html>
