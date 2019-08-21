2011b0c981e0b0d86f05d36f1ae9062a7d8891f9
Pritom Dutta <duttapritom162@gmail.com>
2019-08-21 16:24:12 +0600

    Admin Home update

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,12 +7,16 @@
 		
 		<h1 ><a>Welcome To Admin Panel</a></h1>
 		
-		<h1><a href="/adminhome/adminprofile">Admin Profile</a> | |
-		<a href="/adminhome/moderatorlist">Moderator List</a> ||
-		<a href="/adminhome/addmoderator"> Add Moderator</a> ||
-		<a href="/adminhome/content"> Upload Content</a> ||
-		<a href="/adminhome/movielist"> Delete Content</a> ||
-		<a href="/adminhome/viewrequest"> View Request </a> ||
-		<a href="/logout">LogOut</h1>
+		<h1>
+			<a href="/adminhome/adminprofile">Admin Profile</a> ||
+			<a href="/adminhome/moderatorlist">Moderator List</a> ||
+			<a href="/adminhome/addmoderator"> Add Moderator</a> ||
+			<a href="/adminhome/content"> Upload Content</a> ||
+			<a href="/adminhome/movielist"> Delete Content</a> ||
+			<a href="/adminhome/viewrequest"> View Request </a> ||
+			<a href="/logout">LogOut </a> || 
+			<a href="{{ route('user-management.index') }}"> Back
+
+		</h1>
 	</body>
 	</html>
